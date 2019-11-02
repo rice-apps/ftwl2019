52fbe43a38b283ab85e0a959a8081ed6327e177d
Jonathan Cai <caimjonathan@gmail.com>
2019-11-02 13:03:58 -0500

    Implemented how you can help page

--- components/SmallCard.js
+++ components/SmallCard.js
@@ -5,31 +5,20 @@ function SmallCard(props) {
     return (
         <div style={{height: 300, width: 200, textAlign:"center"}}>
             <Card width={200}>
-                <div style= {{
-                    backgroundColor: props.color,
-                    borderRadius: 10,
-                    height: 200,
-                    width: 200,
-                    margin: "auto"
-                }}> 
                 <Image src={props.image} sx={{margin:"auto", paddingTop: 50}}/>
-                </div>
-
                 <Heading
-                sx={{
-                    fontFamily: 'Helvetica',
-                    fontWeight: 500,       
-                    margin: "auto"   
-                }}>
+                    sx={{
+                        fontFamily: 'Helvetica',
+                        fontWeight: 500,       
+                        margin: "auto"   
+                    }}>
                     {props.heading}
-                    </Heading>
-
-                    <Text
-                        fontSize={[ 1, 1, 2 ]}
-                        color='primary'>
-                        {props.description}
-                    </Text>
-
+                </Heading>
+                <Text
+                    fontSize={[ 1, 1, 2 ]}
+                    color='primary'>
+                    {props.description}
+                </Text>
             </Card>
         </div>
     )
