11cb10a0657606f14b77a750115b6f5136cdac05
janetlu <janet.lu2019@gmail>
2019-11-16 15:00:27 -0600

    Worked on the Support Us While Shopping page and fixed the title.

--- pages/contactus.js
+++ pages/contactus.js
@@ -1,18 +1,18 @@
-import Form from '../components/contact/Form.js'
-import Navbar from '../components/navbar/Navbar.js'
-import Footer from '../components/footer/Footer.js'
-import Title from '../components/util/Title.js'
-import SmallCard from '../components/util/SmallCard.js'
+import Form from '../components/contact/Form.js';
+import Navbar from '../components/navbar/Navbar.js';
+import Footer from '../components/footer/Footer.js';
+import Title from '../components/util/Title.js';
+import SmallCard from '../components/util/SmallCard.js';
 
 function Contact() {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <Title title="Contact Us"></Title>
-            <Form></Form>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Title title={'Contact Us'}></Title>
+      <Form></Form>
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default Contact;
