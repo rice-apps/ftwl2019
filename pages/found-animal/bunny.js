dffc78c3f2db43e6e1b01b28bf789f92db86085c
Anna Bai <annabai@Annas-MacBook-Pro-2.local>
2019-11-16 14:26:46 -0600

    fixed Creature import line

--- pages/found-animal/bunny.js
+++ pages/found-animal/bunny.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import TextImage from '../components/found-animal-single/TextImage.js';
-import Navbar from '../components/navbar/Navbar.js';
-import Footer from '../components/footer/Footer.js';
-import Animal from '../components/found-animal-single/Animal.js';
-import Title from '../components/util/Title.js';
-import Creature from '../components/found-animal-single/Bunny.json';
+import TextImage from '../../components/found-animal-single/TextImage.js';
+import Navbar from '../../components/navbar/Navbar.js';
+import Footer from '../../components/footer/Footer.js';
+import Animal from '../../components/found-animal-single/Animal.js';
+import Title from '../../components/util/Title.js';
+import Creature from '../../components/found-animal-single/Bunny.json';
 
 
 function FoundAnimal() {
