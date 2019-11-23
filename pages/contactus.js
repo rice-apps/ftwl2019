bfe02e5ca767c3d9430f92377b5fd8b198c14439
Vivian Ellis <vce1@rice.edu>
2019-11-23 15:03:24 -0600

    improved host a party formatting

--- pages/contactus.js
+++ pages/contactus.js
@@ -2,17 +2,16 @@ import ContactContent from '../components/contact/ContactContent'
 import Navbar from '../components/navbar/Navbar.js'
 import Footer from '../components/footer/Footer.js'
 import Title from '../components/util/Title.js'
-import SmallCard from '../components/util/SmallCard.js'
 
 function Contact() {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <Title title="Contact Us"></Title>
-            <ContactContent></ContactContent>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Title title="Contact Us"></Title>
+      <ContactContent></ContactContent>
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default Contact;
