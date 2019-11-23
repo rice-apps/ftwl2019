ae9300b7a391af40fef1fd115bac8b5da1603417
Jonathan Cai <caimjonathan@gmail.com>
2019-11-23 14:10:55 -0600

    add pages for navbar

---
+++ pages/donate-supplies.js
@@ -0,0 +1,15 @@
+import Navbar from '../components/navbar/Navbar.js'
+import Footer from '../components/footer/Footer.js'
+import Title from '../components/util/Title.js'
+
+function DonateSupplies() {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Title title="Donate Supplies"></Title>
+            <Footer></Footer>
+        </div>
+    );
+}
+
+export default DonateSupplies;
---
+++ pages/education-and-outreach.js
@@ -0,0 +1,15 @@
+import Navbar from '../components/navbar/Navbar.js'
+import Footer from '../components/footer/Footer.js'
+import Title from '../components/util/Title.js'
+
+function EducationAndOutreach() {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Title title="Education and Outreach"></Title>
+            <Footer></Footer>
+        </div>
+    );
+}
+
+export default EducationAndOutreach;
