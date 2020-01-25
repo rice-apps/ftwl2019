29512063d807bc0b73e4c1311b513e730d97c69b
Anna Bai <annabai@annas-mbp-2.dyndns.rice.edu>
2020-01-25 13:52:57 -0600

    created education/community outreach page using TextImage and a new json file

--- pages/education-and-outreach.js
+++ pages/education-and-outreach.js
@@ -1,12 +1,15 @@
 import Navbar from '../components/navbar/Navbar.js'
 import Footer from '../components/footer/Footer.js'
 import Title from '../components/util/Title.js'
+import Animal from '../components/found-animal-single/Animal.js'
+import Community from '../components/community-outreach-info/community.json'
 
 function EducationAndOutreach() {
     return (
         <div>
             <Navbar></Navbar>
-            <Title title="Education and Outreach"></Title>
+            <Title title="Education and Community Outreach"></Title>
+            <Animal animal={Community}/>
             <Footer></Footer>
         </div>
     );
