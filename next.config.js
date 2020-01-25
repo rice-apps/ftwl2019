83e3b0dc7b786b66212e7b762ad5b0e9df13c748
shaqque <shaqqywyan@gmail.com>
2020-01-25 13:55:29 -0600

    revert changes

--- next.config.js
+++ next.config.js
@@ -1,16 +1,10 @@
 // next.config.js
-const isProduction = process.env.NODE_ENV === 'production';
 const withCSS = require('@zeit/next-css');
 const withImages = require('next-images');
 module.exports = withCSS(
   withImages({
     webpack(config, options) {
       return config;
-    },
-    assetPrefix: isProduction ? '/ftwl2019' : '',
-    publicRuntimeConfig: {
-      // used in '/components/Link.js/', for more details go to the component itself
-      linkPrefix: isProduction ? '/ftwl2019' : '',
     },
   })
 );
