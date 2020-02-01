78d8f5e5cdf39bc66664e75966253c93678141ce
Newton Huynh <nth8@rice.edu>
2020-02-01 15:03:09 -0600

    Created a file for the Learn More Button

---
+++ components/util/LearnMoreButton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from 'rebass';
+import './LearnMoreButton.css'
+ 
+function LearnMoreButton(props) {
+    return (
+        <div>
+            <Button className="LearnMoreButton">
+                LEARN MORE
+            </Button>
+        </div>
+    );
+
+}
