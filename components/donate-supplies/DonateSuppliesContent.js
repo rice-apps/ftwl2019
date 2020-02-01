97cb00f8564ebf5bf59c48148d539673c42e3a83
Vivian Ellis <vce1@rice.edu>
2020-02-01 15:05:49 -0600

    worked on converting supplies collapsible to JSON

--- components/donate-supplies/DonateSuppliesContent.js
+++ components/donate-supplies/DonateSuppliesContent.js
@@ -1,27 +1,29 @@
-// import React from 'react';
-// import { Box, Flex } from 'rebass';
-// import Collapsible from 'react-collapsible';
+import React from 'react';
+import { Box, Flex } from 'rebass';
+import Collapsible from 'react-collapsible';
+import SuppliesCollapsible from './SuppliesCollapsible.js';
+import CollapsibleContent from './CollapsibleContent.json';
 
-// function DonateSuppliesContent() {
-//     return (
-//         <div>
-//             <Flex align="center">
-//                 <Box width={1 / 5}></Box>
-//                 <Box width={1 / 2}>
-//                     <p>
-//                         We are in need of many items to keep the animals happy and healthy. These are
-//                         just some of the items we use on a daily basis.
-//                 </p>
-//                     <p>
-//                         To donate items, visit <u>our rehab center</u> during our business hours or <u>contact us</u> to arrage a donation.
-//                 </p>
-//                 </Box>
-//                 <Box width={1 / 5}></Box>
-//             </Flex>
-
+function DonateSuppliesContent() {
+    return (
+        <div>
             <Flex align="center">
                 <Box width={1 / 5}></Box>
-                <Collapsible open="true">
+                <Box width={1 / 2}>
+                    <p>
+                        We are in need of many items to keep the animals happy and healthy. These are
+                        just some of the items we use on a daily basis.
+                 </p>
+                    <p>
+                        To donate items, visit <u>our rehab center</u> during our business hours or <u>contact us</u> to arrage a donation.
+                 </p>
+                </Box>
+                <Box width={1 / 5}></Box>
+            </Flex>
+            <Flex align="center">
+                <Box width={1 / 5}></Box>
+                <SuppliesCollapsible supplies={CollapsibleContent}></SuppliesCollapsible>
+                {/* <Collapsible open="true">
                     <Collapsible trigger="Animal Food (<)" triggerStyle={{ color: "green" }}>
                         <ul>
                             <li>Science Diet Feline Maintenance Light dry cat food</li>
@@ -39,7 +41,7 @@
                         <p>Laundry detergent</p>
                         <p>...</p>
                     </Collapsible>
-                </Collapsible>
+                </Collapsible> */}
                 <Box width={1 / 5}></Box>
             </Flex>
         </div>
