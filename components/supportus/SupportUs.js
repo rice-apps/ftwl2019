150e147d8831ff369c02f26ee9f8694958e6d633
janetlu <janet.lu2019@gmail>
2020-02-01 15:03:50 -0600

    Worked on changing box color, text alignment, and arrow placement on the Support Us Page.

--- components/supportus/SupportUs.js
+++ components/supportus/SupportUs.js
@@ -2,18 +2,16 @@ import React from 'react';
 import './SupportUs.css';
 import { text } from './SupportUs.json';
 import SupportUsBox from './SupportUsBox.js';
-import Arrow from '../../static/AffProgramsIcon.png';
 import Title from '../util/Title';
 
 function SupportUs(props) {
   return (
-    <div>
+    <div classname="SupportUsBody">
       <Title title={'Support Us While Shopping'}></Title>
       {text.map(t => {
         return (
           <div>
-            <SupportUsBox cardcolor={'green'} title={t.title} text={t.text} />
-            <img className="AffIcon" src={Arrow} />
+            <SupportUsBox cardcolor={t.cardcolor} title={t.title} text={t.text} />
           </div>
         );
       })}
--- components/supportus/SupportUsBox.js
+++ components/supportus/SupportUsBox.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Box, Flex, Image, Card } from 'rebass';
+import { Card } from 'rebass';
 import './SupportUsBox.css';
+import Arrow from '../../static/AffProgramsIcon.png';
 import { loadGetInitialProps } from 'next/dist/next-server/lib/utils';
 
+('Current Issues: Need to move the arrow to the far right. Figure out font sizes. Figure out how to vary text color');
+('Since right now it just says color. Learn to vary border color. ');
 function SupportUsBox(props) {
   return (
-    <div>
-      <Card className="Box" width={1000} height={150} bg={props.cardcolor}>
-        <h1>{props.title}</h1>
-        <h1>{props.text}</h1>
+    <div className="Cards">
+      <Card width={1000} height={125} bg={props.cardcolor} color={props.color}>
+        <h1 className="Title">{props.title}</h1>
+        <h1 className="Subtitle">{props.text}</h1>
+        <img className="AffIcon" src={Arrow} />
       </Card>
     </div>
   );
