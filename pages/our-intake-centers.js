f0b5491ffbad9bea1a1efe7ac3a93d711a7b0601
Elena Margolin <elenamargolin@elenas-mbp.dyndns.rice.edu>
2020-02-01 15:01:29 -0600

    elena stuff!

--- pages/our-intake-centers.js
+++ pages/our-intake-centers.js
@@ -1,17 +1,19 @@
-import Navbar from '../components/navbar/Navbar.js'
-import Footer from '../components/footer/Footer.js'
-import Title from '../components/util/Title.js'
-import CallUsBox from '../components/util/CallUsBox.js'
+import Navbar from '../components/navbar/Navbar.js';
+import Footer from '../components/footer/Footer.js';
+import Title from '../components/util/Title.js';
+import IntakeCenter from '../components/intake-center/IntakeCenter';
+import VisitorCenter from '../components/intake-center/VisitorCenter';
 
 function OurIntakeCenters() {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <Title title="Our Intake Centers"></Title>
-            <CallUsBox></CallUsBox>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Title title="Our Intake Center"></Title>
+      <IntakeCenter></IntakeCenter>
+      <VisitorCenter></VisitorCenter>
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default OurIntakeCenters;
