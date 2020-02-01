fea285602e617918637f7b2cc29e2f78e54e659f
Anna Bai <annabai@annas-mbp-2.dyndns.rice.edu>
2020-02-01 14:49:36 -0600

    added collapsible rehabilitor button on volunteer page

--- components/volunteer/VolunteerHome.js
+++ components/volunteer/VolunteerHome.js
@@ -2,6 +2,7 @@ import Title from '../util/Title.js';
 import SubText from '../subtext/subtext.js';
 import ColoredCard from '../util/ColoredCard.js';
 import {Flex, Box, Button, Image} from 'rebass';
+import Collapsible from 'react-collapsible';
 
 
 function VolunteerHome() {
@@ -11,7 +12,12 @@ function VolunteerHome() {
       <SubText text="Friends of Texas Wildlife has no paid staff and relies totally on caring volunteers to help fulfill its mission. Our volunteers fall under two categories:" />
       <Flex>
           <Box width={1/9}></Box>
-          <Box width={3/9}><ColoredCard line1text="REHABILITATORS" cardcolor="orange"/></Box>
+          <Box width={3/9}>
+            <Collapsible trigger="Rehabilitator">
+              <p>Rehabilitator</p>
+              {/* <ColoredCard line1text="REHABILITATORS" cardcolor="orange"/> */}
+            </Collapsible>
+          </Box>
           <Box width={1/9}></Box>
           <Box width={3/9}><ColoredCard line1text="ADMINISTRATORS" cardcolor="orange"/></Box>
           <Box width={1/9}></Box>
