85b01612fdbcd7a8855d5e126a24db3dd52272f3
Newton Huynh <nth8@rice.edu>
2020-02-08 14:45:42 -0600

    Created a LearnMore button component that can be implemented on any page

--- components/util/LearnMoreButton.js
+++ components/util/LearnMoreButton.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { Button } from 'rebass';
+import { Button, Box, Border } from 'rebass';
 import './LearnMoreButton.css'
  
 function LearnMoreButton(props) {
     return (
         <div>
-            <Button className="LearnMoreButton">
+
+            <Button
+            width={240}
+            height={60}
+            sx={{border: '3px solid green'}}
+            classname="learn-more-button"
+            >
+                <b className="learn-more-text">
                 LEARN MORE
+                </b>
+
             </Button>
+
         </div>
     );
 
 }
+
+export default LearnMoreButton; 
