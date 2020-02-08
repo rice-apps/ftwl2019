62018a6211897e912da3922ae7a27f75960a7cae
janetlu <janet.lu2019@gmail>
2020-02-08 15:39:38 -0600

    Compiled colored cards into the board members page

---
+++ components/boardmembers/BoardMembers.js
@@ -0,0 +1,69 @@
+import Title from '../util/Title';
+import ColoredCard from '../util/ColoredCard';
+import './BoardMembers.css';
+import BoardMembersjson from './BoardMembers.json';
+
+function BoardMembers() {
+  return (
+    <div>
+      <Title
+        title="Our Board Members"
+        subtitle="Our Board of Directors plays a critical role in guiding Friends of Texas Wildlife to meet the needs of the community."
+      />
+      <Title title="Board Officers" />
+
+      {BoardMembersjson.boardofficers.map(q => {
+        return (
+          <div>
+            <div className="BoardOfficers">
+              <ColoredCard
+                className="Cards"
+                cardwidth="400px"
+                cardcolor="orange"
+                line1text={q.position}
+                line2text={q.name}
+              />
+            </div>
+          </div>
+        );
+      })}
+
+      <Title title="Board Members" />
+
+      {BoardMembersjson.boardmembers.map(i => {
+        return (
+          <div>
+            <div className="BoardMembers">
+              <ColoredCard
+                className="Cards"
+                cardwidth="400px"
+                cardcolor="orange"
+                line1text={i.position}
+                line2text={i.name}
+              />
+            </div>
+          </div>
+        );
+      })}
+      <Title title="Advisory Board Members" />
+
+      {BoardMembersjson.advisoryboard.map(j => {
+        return (
+          <div>
+            <div className="AdvisoryBoard">
+              <ColoredCard
+                className="Cards"
+                cardwidth="400px"
+                cardcolor="orange"
+                line1text={j.position}
+                line2text={j.name}
+              />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default BoardMembers;
