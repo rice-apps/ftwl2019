d80cef6185dfde962cfadb48a6e4c2104707a555
Jonathan Cai <caimjonathan@gmail.com>
2020-02-15 16:44:57 -0600

    contentful working for faq retrieving one entry linked to all q&a entries

--- components/faq/TableText.js
+++ components/faq/TableText.js
@@ -1,37 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import { Box, Flex, Image } from 'rebass';
 import './TableText.css';
 import FAQjson from './FAQ.json';
+import client from '../../client';
 
 function TableText() {
 
+    // Declare the use of FAQ as a state object
+    const [FAQ, setFAQ] = useState([]);
+
+    // Use effect with (func, empty array) means that func gets called when this
+    // component is rendered.
+    useEffect(() => {
+        // Use client object to retrieve an entry
+        client.getEntry('2XgFSQ5SEmUVz6JLeUTQOw')
+        .then(function (entry) {
+            console.log(entry.fields.questions);
+            setFAQ(entry.fields.questions);
+        });
+    }, [])
+
     return (
         <div className="TableText">
-            {
-                FAQjson.faq.map(
-                    q => {
+             {
+                FAQ.map(
+                    (q, idx1, arr) => {
+                        console.log(q.fields);
                         return (
-                        <Flex>
+                        <Flex key={idx1}>
                             <Box width={1/5}></Box>
                             <Box width={3/20}>
-                                <p><b>{q.sideQuestion}</b></p>
+                                <p><b>{q.fields.topic}</b></p>
                             </Box>
                             <Box width={1/20}></Box>
                             <Box width={2/5}>
-                                <p><b>{q.question}</b></p>
-                                {q.answer.map(
-                                    a => {return (
-                                        <p>{a}</p>
+                                <p><b>{q.fields.question}</b></p>
+                                
+                                {/* Split by \n separates the string
+                                    into a list of strings separated by
+                                    the paragraphs. */}
+                                {q.fields.answer.split('\n').map(
+                                    (a, idx2) => {return (
+                                        <p key={idx2}>{a}</p>
                                     );
                                     }
                                 )}
+
                             </Box>
                             <Box width = {1/5}></Box>
 
                         </Flex>);
                     }
                 )
-            }
+             }
         </div>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -5,18 +5,8 @@ import MainDeer from '../components/splashpage/MainDeer';
 import NeedAssistance from '../components/splashpage/NeedAssistance';
 import WaysToGive2 from '../components/splashpage/WaysToGive2';
 import Link from 'next/link';
-import client from '../client';
 
 function Home() {
-
-  client.getEntry('2XgFSQ5SEmUVz6JLeUTQOw')
-  .then(function (entry) {
-    // logs the entry metadata
-    // console.log(entry.sys);
-
-    // logs the field with ID title
-    console.log(entry.fields.questions);
-  })
 
   return (
     <div>
