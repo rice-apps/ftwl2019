86492e34e7310d360244da43b2dd5ede0ec45681
Newton Huynh <nth8@rice.edu>
2020-02-22 15:30:48 -0600

    Making Donate Supply page retrieve data from contentful

--- components/donate-supplies/SuppliesCollapsible.js
+++ components/donate-supplies/SuppliesCollapsible.js
@@ -1,16 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
 import Collapsible from 'react-collapsible';
+import client from '../../client';
+import { Box, Flex } from 'rebass';
+import "./DonateSuppliesContent.css";
 
 function SuppliesCollapsible(props) {
+
+    const [DS, setDS] = useState([]);
+
+    useEffect(() => {
+        client.getEntries({
+            'content_type': 'donateSupplyCategory'
+        })
+        .then(function (entries) {
+            entries.items.reverse();
+            setDS(entries.items);
+
+        })
+
+    }, [])
+
+
+    // return (
+    //     <div>
+    //         {props.supplies.sections.map(section => {
+    //             return <Collapsible trigger={section.title}>
+    //                 {section.rows}
+    //             </Collapsible>
+    //         }
+    //         )}
+    //     </div>
+
+
     return (
         <div>
-            {props.supplies.sections.map(section => {
-                return <Collapsible trigger={section.title}>
-                    {section.rows}
-                </Collapsible>
+            {
+                DS.map(
+                    q => {
+                        return (
+                        <Flex className="SupplyList">
+                            <Box width="750px" bg="green"> {q.fields.category} </Box>
+                            {q.fields.supplyList.split(('\n')).map(
+                                a => {return(
+                                    <Box align="center" width={2/3}>
+                                        {a}
+                                    </Box>)
+                                }
+                            )
+                            }
+                        </Flex>
+                        )
+                    }
+
+
+                )
             }
-            )}
+            
         </div>
-    );
+    )
+    
 }
 export default SuppliesCollapsible;
