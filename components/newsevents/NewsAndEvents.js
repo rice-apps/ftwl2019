92ecddda60caf67158b6f9a7215e89a22176fcd1
shaqque <shaqqywyan@gmail.com>
2020-02-22 15:04:01 -0600

    add skeleton for all news and events

---
+++ components/newsevents/NewsAndEvents.js
@@ -0,0 +1,8 @@
+import Title from '../util/Title';
+import ImageText from '../util/ImageText';
+
+function NewsAndEventsBody() {
+  return <ImageText title="April Fundraiser" date="Thursday, April 12, 2019" preview="hello this is really a short preview of our blog post." />;
+}
+
+export default NewsAndEventsBody;
---
+++ pages/news-and-events.js
@@ -0,0 +1,15 @@
+import NewsAndEventsBody from '../components/newsevents/NewsAndEvents';
+import Navbar from '../components/navbar/Navbar';
+import Footer from '../components/footer/Footer';
+
+function NewsAndEvents() {
+  return (
+    <div>
+      <Navbar />
+      <NewsAndEventsBody />
+      <Footer />
+    </div>
+  );
+}
+
+export default NewsAndEvents;
