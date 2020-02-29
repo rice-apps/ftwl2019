89e126b44ac0439a99a5540b5ccf419805132299
Anna Bai <annabai@annas-mbp-2.dyndns.rice.edu>
2020-02-29 15:28:31 -0600

    added news and events page that updates from contentful

--- components/newsevents/NewsAndEvents.js
+++ components/newsevents/NewsAndEvents.js
@@ -1,25 +1,35 @@
 import Title from '../util/Title';
 import ImageText from '../util/ImageText';
+import React, {useState, useEffect} from "react";
 
-newsAndEventsJSON = {
-  items: [
-    {
-      title: 'April Fundraiser',
-      date: 'Thursday, April 12, 2019',
-      preview: 'hello this is really a short preview of our blog post.',
-    },
-  ],
-};
+import client from '../../client.js';
 
 function NewsAndEventsBody() {
   // return <ImageText title="April Fundraiser" date="Thursday, April 12, 2019" preview="hello this is really a short preview of our blog post." />;
+  const [event, setEvent] = useState([]);
+
+  useEffect(() => {
+    client.getEntries({
+      links_to_entry: '6230e7e84ad1RzTUayOhBj'
+    })
+    .then( function (response) {
+      console.log(response);
+      response.items.reverse();
+      setEvent(response.items);
+    })
+  },[])
+  
+  console.log(event)
+
   return (
     <div>
-      {newsAndEventsJSON.items.map(x => {
-        <ImageText title={x.title} date={x.date} preview={x.preview} />
+      {event.map(x => {
+        console.log(x.fields.announcementTitle);
+        return <ImageText title={x.fields.announcementTitle} />
       })}
     </div>
   );
+  
 }
 
 export default NewsAndEventsBody;
