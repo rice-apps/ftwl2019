c239363b6ba4eb55ebd634c6d7e395a7ecde483d
Jonathan Cai <caimjonathan@gmail.com>
2020-03-07 14:30:46 -0600

    store opens new tab

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -120,7 +120,7 @@ function Navbar() {
       {navbar_headers.map(({ name, to, subheaders, absoluteTo }) => (
         <div className="navbar-category" key={name}>
           {absoluteTo ? (
-            <a href={absoluteTo} className="navbar-dropdown-absolute">
+            <a href={absoluteTo} target="_blank" className="navbar-dropdown-absolute">
               <button className="navbar-dropdown-opener">{name}</button>
             </a>
           ) : (
