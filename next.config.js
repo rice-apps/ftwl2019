87a9af4d8d481e2e49c4f5fd0d5111b86944cadb
shaqque <shaqqywyan@gmail.com>
2020-03-07 14:21:06 -0600

    enable font loading and add Kollektif

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 // next.config.js
 const withCSS = require('@zeit/next-css');
 const withImages = require('next-images');
+const withFonts = require('next-fonts');
 module.exports = withCSS(
-  withImages({
-    webpack(config, options) {
-      return config;
-    },
-  })
+  withImages(
+    withFonts({
+      webpack(config, options) {
+        return config;
+      },
+    })
+  )
 );
